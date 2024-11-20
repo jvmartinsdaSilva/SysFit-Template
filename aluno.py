b8902a27fb0f5bdaae608cc72b36fa78b275d65d
jvmartinsdaSilva <jvmartins0855@gmail.com>
2024-11-20 17:42:41 -0300

    Criação da função de para cadastro de alunos

--- aluno.py
+++ aluno.py
@@ -1,1 +1,20 @@
 from entrada import *
+
+
+def cadastro_aluno(id):
+    nome = input("Informe o nome do aluno: ")
+    contato = input("Infome seu e-mail: ")
+    idade = ler_inteiro("Informe a idade do aluno: ", True)
+    peso = ler_real("Informe o peso do aluno: ", True)
+    altura = ler_real("Informe a altura: ", True)
+
+    aluno = {
+        "id": id,
+        "nome": nome ,
+        "contato": contato,
+        "idade":  idade,
+        "peso": peso,
+        "altura": altura,
+        "IMC": peso / altura
+    }
+    return aluno
