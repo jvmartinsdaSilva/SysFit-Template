4e3800148408c660b37cff729f79dceba4e5bb41
jvmartinsdaSilva <jvmartins0855@gmail.com>
2024-11-22 18:40:23 -0300

    Substituição funções de busca por IMC e id, por uma geral

--- aluno.py
+++ aluno.py
@@ -17,32 +17,31 @@ def cadastro_aluno(id):
         "peso": peso,
         "altura": altura,
         "IMC": round((peso / altura), 2)
-
     }
     print(aluno)
     return aluno
 
+
 def mostrar_alunos(alunos):
     for aluno in alunos:
         print(aluno)
+
 
 def salvar_alunos(alunos):
     alunos_salvos = buscar_todos_alunos_em_arquivo() # Busca se já existem alunos salvos
     novos_alunos = alunos_salvos + alunos # Junta os alunos antigos com os novos
     salvar_alunos_em_arquivo(novos_alunos)
 
-def buscar_aluno_por_id(alunos):
-    id = ler_inteiro("Informe o Id do aluno desejado: ", True)
-    for aluno in alunos:
-        if(aluno['id'] == id): return aluno
-    return f"Aluno com o id: {id} não encontrado"
-    
 
-def buscar_aluno_por_IMC(alunos):
-    imc = ler_real("Informe o valor do IMC desejado", True)
+def buscar_aluno_por_tag(tag , valor, alunos): # Buscar valor na lista de alunos salvas com base na tag
+    # TAG -> Tipo do parametro desejado na busca (nome, id, IMC, etc...)
+    # Valor -> Valor que sera buscado na lista de alunos
+    alunos_validos = []
     for aluno in alunos:
-        if(aluno['IMC'] == imc): return aluno
-    return f"Aluno com o IMC: {imc} não encontrado"
+        if(aluno[tag] == valor): alunos_validos.append(aluno)
+    
+    if len(alunos_validos) >= 1: return alunos_validos # Se houver retorna lista de alunos
+    else: return f"Nenhum Aluno com {tag} = {valor} encontrado"
 
 
 def buscar_id_ultimo_aluno(alunos):
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from navegabilidade import *
 from gerenciadorJSON import *
 
 #variáveis de controle dos dados na memória
-id = buscar_id_ultimo_aluno(buscar_todos_alunos_em_arquivo())
+id = buscar_id_ultimo_aluno(buscar_todos_alunos_em_arquivo()) # Define qual deve ser o próximo id ao inciar o programa
 """ número do id de autoincremento
 """
 alunos = []
@@ -22,14 +22,16 @@ while True:
         id += 1
         pass
     elif (opc == 2): # Mostra todos alunos na tela
-        mostrar_alunos(buscar_todos_alunos_em_arquivo())
+        mostrar_alunos(buscar_todos_alunos_em_arquivo() + alunos)
         pass
     elif (opc == 3): #busca um aluno por id e apresenta seus dados se existir
-        aluno = buscar_aluno_por_id(buscar_todos_alunos_em_arquivo() + alunos)
+        id_aluno = ler_inteiro("Informe o ID desejado: ", True)
+        aluno = buscar_aluno_por_tag("id", id_aluno, buscar_todos_alunos_em_arquivo() + alunos)
         print(aluno)
         pass
     elif (opc == 4): #exibe uma lista com todos os alunos filtrados por imc
-        aluno = buscar_aluno_por_IMC(buscar_todos_alunos_em_arquivo() + alunos)
+        imc_aluno = ler_real("Informe o IMC do aluno: ", True)
+        aluno = buscar_aluno_por_tag("IMC", imc_aluno, buscar_todos_alunos_em_arquivo() + alunos)
         print(aluno)
         pass
     elif (opc == 5):
